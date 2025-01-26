7fefa7e7628e120c3b96d531d8917d9d23aa387a
Ostrowski_Marek <ostrowski.marek@boz.pl>
2025-01-26 21:04:09 +0100

    Player works moves up down left right

--- main.py
+++ main.py
@@ -3,20 +3,41 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
+    updateable = pygame.sprite.Group(player)
+    drawable = pygame.sprite.Group(player)
+
+    Player.containers = (updateable, drawable)
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0), rect=None, special_flags=0)
+
+        for thing in updateable:
+            thing.update(dt)
+
+        screen.fill("black")
+
+        for thing in drawable:
+           thing.draw(screen)
+
         pygame.display.flip()
+        
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
